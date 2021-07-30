c9c97c10e58fb000dabaf8c9d166d4ae41d8d51b
Durso <dursozd@gmail.com>
2021-07-29 22:33:38 -0400

    week 2

---
+++ notes/ascii.c
@@ -0,0 +1,23 @@
+/*
+#include <stdio.h>
+#include <cs50.h>
+
+int main(void)
+{
+
+    string message = get_string("Message: ");
+
+    for (int i = 0; i < message[i] != '\0'; i++)
+    {
+    printf("%i \n", message[i]);
+    }
+
+}
+
+
+
+// This was a demonstration using the hash symbol, but I thought it would be really cool to be able to get the ascii code for any character. So I did this.
+
+// turns out he did this exact thing, pretty much. just using char to output the same string instead of the ascii number
+
+*/
